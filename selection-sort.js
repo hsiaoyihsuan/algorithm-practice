87d5e7a65d5e231438982cdc85db8a63d954538c
Hsuan19971005 <hsuan9705@gmail.com>
2024-06-22 17:07:30 +0800

    docs: Add illustrations to selection sort

--- selection-sort.js
+++ selection-sort.js
@@ -1,15 +1,17 @@
-let arr = [8, 5, 7, 6];
+let arr = [3, 2, 4, 5, 8, 1];
 function selectionSort(arr) {
+  // Fint min n times
   for (let i = 0; i < arr.length; i++) {
     let minIndex = i;
+    // Loop the rest of elements to find the min number
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[j] < arr[minIndex]) {
         minIndex = j;
       }
     }
-    [arr[minIndex], arr[i]] = [arr[i], arr[minIndex]];
+    // After finding the min value, swap the min value with current i position
+    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
   }
   return arr;
 }
-
 console.log(selectionSort(arr));
