5ae06ae111c7893b0868fb45ca75b0004acbe9b8
Hsuan19971005 <hsuan9705@gmail.com>
2024-06-30 14:17:42 +0800

    fix: Fix binary search method using error

--- binary-search.js
+++ binary-search.js
@@ -1,22 +1,23 @@
-function binarySearchRecursive(arr, target, start, end) {
-  if (start > end) {
+function binarySearchRecursive(arr, target, left, right) {
+  if (left > right) {
     return -1; // not found
   }
 
-  const mid = Math.floor((start + end) / 2);
+  const mid = Math.floor((left + right) / 2);
   if (arr[mid] === target) {
     return mid; // found
   } else if (arr[mid] < target) {
-    return binarySearchRecursive(arr, target, mid + 1, end);
+    return binarySearchRecursive(arr, target, mid + 1, right);
   } else {
-    return binarySearchRecursive(arr, target, start, mid - 1);
+    return binarySearchRecursive(arr, target, left, mid - 1);
   }
 }
+
 let arr1 = [0, 2, 3, 6, 8, 12, 38, 81, 399];
-console.log(binarySearchIterative(arr1, 3));
-console.log(binarySearchIterative(arr1, 81));
-console.log(binarySearchIterative(arr1, 8));
-console.log(binarySearchIterative(arr1, 100));
+console.log(binarySearchRecursive(arr1, 3, 0, arr1.length));
+console.log(binarySearchRecursive(arr1, 81, 0, arr1.length));
+console.log(binarySearchRecursive(arr1, 8, 0, arr1.length));
+console.log(binarySearchRecursive(arr1, 100, 0, arr1.length));
 
 function binarySearchIterative(arr, target) {
   let left = 0;
