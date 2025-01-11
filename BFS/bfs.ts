7b1cbb807fa1ffde188def42e289752cabac716b
Hsuan19971005 <hsuan9705@gmail.com>
2025-01-11 15:30:38 +0800

    Add bfsRecursive

--- BFS/bfs.ts
+++ BFS/bfs.ts
@@ -1,4 +1,4 @@
-const graph = new Map<string, string[]>([
+export const graph = new Map<string, string[]>([
   ["A", ["B", "C"]],
   ["B", ["A", "D", "E"]],
   ["C", ["A", "F"]],
@@ -7,7 +7,7 @@ const graph = new Map<string, string[]>([
   ["F", ["C", "E"]],
 ]);
 
-export function bfs(graph: Map<string, string[]>, start: string): void {
+function bfsIterative(graph: Map<string, string[]>, start: string): void {
   const visited = new Set<string>(); // Track visited nodes
   const queue: string[] = [start]; // Queue for BFS initialize with start node
   visited.add(start);
@@ -28,4 +28,25 @@ export function bfs(graph: Map<string, string[]>, start: string): void {
   }
 }
 
-bfs(graph, "A");
+function bfsRecursive(
+  graph: Map<string, string[]>,
+  queue: string[],
+  visited: Set<string>
+) {
+  if (queue.length === 0) return;
+
+  const node = queue.shift()!;
+  console.log(node);
+
+  for (const neighbor of graph.get(node) ?? []) {
+    if (!visited.has(neighbor)) {
+      queue.push(neighbor);
+      visited.add(neighbor);
+    }
+  }
+
+  bfsRecursive(graph, queue, visited);
+}
+
+bfsIterative(graph, "A"); // A B C D E F
+bfsRecursive(graph, ["A"], new Set<string>(["A"])); // A B C D E F
